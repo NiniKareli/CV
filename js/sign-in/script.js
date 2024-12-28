ff7f779be4d1ffc2851150d573f0f23913133eed
Luka Chubinidze <chubinidzel08@gmail.com>
2024-12-28 15:28:09 +0400

    add sign in visual and functionality

---
+++ js/sign-in/script.js
@@ -0,0 +1,40 @@
+// DOM Elements
+const signInForm = document.getElementById('sign-up-form');
+
+// Functions
+async function signIn(e) {
+    e.preventDefault();
+
+    // Get input data
+    const formData = new FormData(signInForm);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    // Validation
+    if (!email || !password) {
+        alert('Please fill out all fields.');
+        return;
+    }
+
+    // Retrieve users data from storage
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    // Check if the user exists
+    const existingUser = users.find(user => user.email === email);4
+
+    if (!existingUser) {
+        alert('Could not find any user related to the email. Try again.');
+    } else {
+        // Check if the password is correct
+        const hashedPassword = await encryptStr(password);
+
+        if (existingUser.password === hashedPassword) {
+            window.location.href = 'index.html';
+        } else {
+            alert('Incorrect password.');
+        }
+    }
+}
+
+// Event listener bindings
+signInForm.addEventListener('submit', signIn);
